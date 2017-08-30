5d6a66b9658eef49c5ff6a1f6147fd9f215c86b0
Lee Jen Huei <jenhuei@luminousgames.co.nz>
2017-08-31 07:10:36 +1200

    S05_L30 Sub-Behavior Tree

--- TestingGround/Source/TestingGround/PatrolRoute.h
+++ TestingGround/Source/TestingGround/PatrolRoute.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+
 
 #pragma once
 
@@ -15,7 +15,7 @@ class TESTINGGROUND_API UPatrolRoute : public UActorComponent
 public:
     TArray<AActor*> GetPatrolPoints() const;
     
-protected:
+private:
     UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
     TArray<AActor*> PatrolPoints = TArray<AActor*>();
 	
